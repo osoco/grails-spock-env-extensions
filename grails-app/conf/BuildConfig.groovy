60867f2f46166d959665ae3b3333afb7a902d7d6
mgryszko <marcin@grysz.com>
2013-09-30 21:59:32 +0200

    release plugin configured

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,18 +1,19 @@
 grails.release.scm.enabled = false
 grails.project.dependency.resolution = {
-    inherits("global") {
+    inherits 'global', {
         excludes 'ehcache'
     }
     repositories {
         grailsPlugins()
         grailsHome()
         grailsCentral()
+        mavenCentral()
     }
     dependencies {
         test 'org.spockframework:spock-core:0.7-groovy-2.0'
     }
     plugins {
-        build(':release:3.0.1', ':rest-client-builder:1.0.3') {
+        build ':release:3.0.1', ':rest-client-builder:1.0.3', {
             export = false
         }
     }
